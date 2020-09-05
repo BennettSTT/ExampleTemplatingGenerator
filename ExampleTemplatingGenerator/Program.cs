5f2a9fcc424cadf1f6434637aa973e3053e1f3f2
Artem Sazonov <bennettst@yandex.ru>
2020-09-05 15:38:36 +0700

    init

---
+++ ExampleTemplatingGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ExampleTemplatingGenerator
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //
+        }
+    }
+}
---
+++ TemplatingGenerator/ClientModelsGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+using Microsoft.VisualStudio.TextTemplating;
+
+namespace TemplatingGenerator
+{
+    public class ClientModelsGenerator
+    {
+        public void Initialize(ITextTemplatingEngineHost host, StringBuilder template)
+        {
+            // initialization of fields
+        }
+
+        public virtual void Generate()
+        {
+            // code generation logic
+        }
+    }
+}
